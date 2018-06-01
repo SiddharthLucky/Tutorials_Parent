689c334bb489135b13f00280b7307a24cda81dfc
=SiddharthLucky <=siddharth.luckyreddy@gmail.com>
2018-06-01 01:11:50 -0500

    Added sql files for classic models

---
+++ SQL_Databases/Customer_SQL.sql
@@ -0,0 +1,6 @@
+SELECT * FROM classicmodels.customers where postalcode = 44000;
+
+SELECT customerName, contactLastName FROM classicmodels.customers where creditLimit > 1000;
+
+SELECT customerName, phone FROM classicmodels.customers 
+
